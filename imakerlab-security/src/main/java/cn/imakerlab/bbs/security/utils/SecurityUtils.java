356a768dd2960ab64989f0c19addd30ab89a96b8
Wings-Liberty <cx1434948003@163.com>
2020-01-30 11:22:22 +0800

    1. security模块的工具类里添加了从request的令牌中获取用户id的方法 2. 修改了之前无法在jwt中添加自定义信息的问题

--- imakerlab-security/src/main/java/cn/imakerlab/bbs/security/utils/SecurityUtils.java
+++ imakerlab-security/src/main/java/cn/imakerlab/bbs/security/utils/SecurityUtils.java
@@ -1,5 +1,35 @@
 package cn.imakerlab.bbs.security.utils;
 
-//自定义的方法，，用于从authentication中获取id
+import cn.imakerlab.bbs.model.exception.MyException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+
 public class SecurityUtils {
+
+    public static int getUserIdFromAuthenticationByRequest(HttpServletRequest request){
+
+        String header = request.getHeader("Authorization");
+
+        //这里的Bearer不能忽略大小写
+        String token = StringUtils.substringAfter(header, "Bearer ");
+
+        Claims claims = null;
+        try {
+            claims = Jwts.parser().setSigningKey("cx".getBytes("UTF-8"))
+                    .parseClaimsJws(token).getBody();
+        } catch (UnsupportedEncodingException e) {
+            throw new MyException("security模块的工具类抛出的UnsupportedEncodingException");
+        }
+
+        System.out.println("claims : " + claims);
+
+        Integer userId =  Integer.parseInt((String) claims.get("userId"));
+
+        return userId;
+    }
+
 }
